dae91e65ce7034315b9cb3d609926cac86f53e1f
masarakki <masaki@hisme.net>
2013-06-28 03:05:43 +0900

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShimakazeBrowser::Application.config.secret_key_base = '07b23546abb1c7bba8b06f3b2d3280b1c36f5cc20827670c3cf3df0211b8f1b5c1a69379ad04662b2aa7fa2a1facfe326ac76f065e0de02ded473c1b576dfdf9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShimakazeBrowser::Application.config.session_store :cookie_store, key: '_shimakaze_browser_session'
